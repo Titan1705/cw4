dcad6506b975576f83e30e5543961fbbd3ad7ef0
Stanley Ipkis <ruben_2015@mail.ru>
2024-04-24 18:05:32 +0800

    private url

--- src/classes/HH_API.py
+++ src/classes/HH_API.py
@@ -9,7 +9,7 @@ class HeadHunterAPI(AbsctractAPI):
     с api.hh.ru/vacancies, для дальнейшей обработки через класс Vacancy"""
 
     def __init__(self):
-        self.url = 'https://api.hh.ru/vacancies'
+        self.__url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
 
     def load_vacancies(self, keyword, page=0, per_page=20):
@@ -17,7 +17,7 @@ class HeadHunterAPI(AbsctractAPI):
         в объявлении стоят именованные аргументы page  per_page,
         чтобы задать значение по умолчанию и сделать возможность вывода
         разного количества вакансий"""
-        response = requests.get(self.url,
+        response = requests.get(self.__url,
                                 params={'text': keyword, 'page': page, 'per_page': per_page},
                                 headers=self.headers)
         response_json = response.json()['items']
